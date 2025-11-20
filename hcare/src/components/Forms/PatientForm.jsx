20e891804ef15dfd3ac91288056fe168b1a98095
A-Athithya <athithya0420@gmail.com>
2025-11-20 19:06:18 +0530

    Forms update

--- hcare/src/components/Forms/PatientForm.jsx
+++ hcare/src/components/Forms/PatientForm.jsx
@@ -9,7 +9,6 @@ import {
   InputNumber,
   message,
 } from "antd";
-import { Collapse } from "antd";
 import dayjs from "dayjs";
 import { useDispatch } from "react-redux";
 import { postData, putData } from "../../api/client";
@@ -20,7 +19,6 @@ const PatientForm = forwardRef(({ initial = null, onSaved = () => {} }, ref) => 
   const [form] = Form.useForm();
   const dispatch = useDispatch();
 
-  // ⭐ Make functions available to parent using ref
   useImperativeHandle(ref, () => ({
     submitForm: () => form.submit(),
     resetForm: () => form.resetFields(),
@@ -60,98 +58,121 @@ const PatientForm = forwardRef(({ initial = null, onSaved = () => {} }, ref) => 
       }
 
       onSaved();
-    } catch (err) {
+    } catch {
       message.error("Save failed");
     }
   };
 
   return (
-      <div style={{ paddingRight: 10 }}>
-        <Form
-          layout="vertical"
-          form={form}
-          onFinish={submit}
-          initialValues={{ gender: "Male", status: "Active" }}
-        >
-          <Collapse defaultActiveKey={["1", "2", "3"]}>
-            
-            <Collapse.Panel header="Basic Details" key="1">
-              <Row gutter={16}>
-                <Col span={12}>
-                  <Form.Item name="name" label="Full Name" rules={[{ required: true }]}>
-                    <Input />
-                  </Form.Item>
-                  <Form.Item name="age" label="Age" rules={[{ required: true }]}>
-                    <InputNumber style={{ width: "100%" }} min={1} />
-                  </Form.Item>
-                  <Form.Item name="gender" label="Gender" rules={[{ required: true }]}>
-                    <Select>
-                      <Select.Option value="Male">Male</Select.Option>
-                      <Select.Option value="Female">Female</Select.Option>
-                      <Select.Option value="Other">Other</Select.Option>
-                    </Select>
-                  </Form.Item>
-                </Col>
+    <div style={{ paddingRight: 8 }}>
+      <Form
+        layout="vertical"
+        form={form}
+        onFinish={submit}
+        initialValues={{ gender: "Male", status: "Active" }}
+        style={{ marginTop: "-10px" }}   // ⭐ reduce top gap
+      >
+        <Row gutter={[12, 4]}>  {/* ⭐ Reduced vertical & horizontal spacing */}
 
-                <Col span={12}>
-                  <Form.Item name="bloodGroup" label="Blood Group">
-                    <Input />
-                  </Form.Item>
-                  <Form.Item name="registeredDate" label="Registered Date">
-                    <DatePicker style={{ width: "100%" }} />
-                  </Form.Item>
-                </Col>
-              </Row>
-            </Collapse.Panel>
+          {/* BASIC FIELDS */}
+          <Col span={8}>
+            <Form.Item name="name" label="Full Name" rules={[{ required: true }]} style={{ marginBottom: 8 }}>
+              <Input />
+            </Form.Item>
+          </Col>
 
-            <Collapse.Panel header="Contact Details" key="2">
-              <Row gutter={16}>
-                <Col span={12}>
-                  <Form.Item name="contact" label="Contact" rules={[{ required: true }]}>
-                    <Input />
-                  </Form.Item>
-                  <Form.Item name="email" label="Email">
-                    <Input />
-                  </Form.Item>
-                </Col>
+          <Col span={8}>
+            <Form.Item name="age" label="Age" rules={[{ required: true }]} style={{ marginBottom: 8 }}>
+              <InputNumber style={{ width: "100%" }} min={1} />
+            </Form.Item>
+          </Col>
 
-                <Col span={12}>
-                  <Form.Item name="address" label="Address">
-                    <Input />
-                  </Form.Item>
-                  <Form.Item name="emergencyContact" label="Emergency Contact">
-                    <Input />
-                  </Form.Item>
-                </Col>
-              </Row>
-            </Collapse.Panel>
+          <Col span={8}>
+            <Form.Item name="gender" label="Gender" rules={[{ required: true }]} style={{ marginBottom: 8 }}>
+              <Select>
+                <Select.Option value="Male">Male</Select.Option>
+                <Select.Option value="Female">Female</Select.Option>
+                <Select.Option value="Other">Other</Select.Option>
+              </Select>
+            </Form.Item>
+          </Col>
 
-            <Collapse.Panel header="Medical Details" key="3">
-              <Row gutter={16}>
-                <Col span={12}>
-                  <Form.Item name="medicalHistory" label="Medical History">
-                    <TextArea rows={3} />
-                  </Form.Item>
-                </Col>
+          {/* CONTACT */}
+          <Col span={8}>
+            <Form.Item name="contact" label="Contact" rules={[{ required: true }]} style={{ marginBottom: 8 }}>
+              <Input />
+            </Form.Item>
+          </Col>
 
-                <Col span={12}>
-                  <Form.Item name="allergies" label="Allergies">
-                    <Input />
-                  </Form.Item>
-                  <Form.Item name="status" label="Status">
-                    <Select>
-                      <Select.Option value="Active">Active</Select.Option>
-                      <Select.Option value="Inactive">Inactive</Select.Option>
-                    </Select>
-                  </Form.Item>
-                </Col>
-              </Row>
-            </Collapse.Panel>
+          <Col span={8}>
+            <Form.Item name="email" label="Email" style={{ marginBottom: 8 }}>
+              <Input />
+            </Form.Item>
+          </Col>
 
-          </Collapse>
-        </Form>
-      </div>
-    );
+          <Col span={8}>
+            <Form.Item name="address" label="Address" style={{ marginBottom: 8 }}>
+              <Input />
+            </Form.Item>
+          </Col>
+
+          {/* PATIENT FIELDS */}
+          <Col span={8}>
+            <Form.Item name="bloodGroup" label="Blood Group" style={{ marginBottom: 8 }}>
+              <Input />
+            </Form.Item>
+          </Col>
+
+          <Col span={8}>
+            <Form.Item name="registeredDate" label="Registered Date" style={{ marginBottom: 8 }}>
+              <DatePicker style={{ width: "100%" }} />
+            </Form.Item>
+          </Col>
+
+          <Col span={8}>
+            <Form.Item name="emergencyContact" label="Emergency Contact" style={{ marginBottom: 8 }}>
+              <Input />
+            </Form.Item>
+          </Col>
+
+          {/* MEDICAL HISTORY LEFT (TALL FIELD) */}
+          <Col span={12}>
+            <Form.Item name="medicalHistory" label="Medical History"
+              style={{ marginBottom: 8 }}
+            >
+              <TextArea
+                style={{
+                  height: 33,        // same as normal input height
+                  resize: "vertical" // allow manual expand
+                }}
+              />
+            </Form.Item>
+          </Col>
+
+          {/* RIGHT SIDE → MAKE IT EVEN */}
+          <Col span={12}>
+            <Row gutter={[12, 4]}>
+              <Col span={24}>
+                <Form.Item name="allergies" label="Allergies" style={{ marginBottom: 8 }}>
+                  <Input />
+                </Form.Item>
+              </Col>
+
+              <Col span={24}>
+                <Form.Item name="status" label="Status" style={{ marginBottom: 0 }}>
+                  <Select>
+                    <Select.Option value="Active">Active</Select.Option>
+                    <Select.Option value="Inactive">Inactive</Select.Option>
+                  </Select>
+                </Form.Item>
+              </Col>
+            </Row>
+          </Col>
+
+        </Row>
+      </Form>
+    </div>
+  );
 });
 
 export default PatientForm;
